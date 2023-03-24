d8cfbf89fc4a7b454a6e360731df864429cefa31
Leonardo <leonardopatino99@gmail.com>
2023-03-23 23:25:21 -0500

    mejora en estilos generales, organización de breakpoints responsive, header más pequeño verticalmente

--- src/components/layout/header/nav.template.js
+++ src/components/layout/header/nav.template.js
@@ -16,10 +16,10 @@ export class NavComponent extends Component{
     return super.template(`
     <nav class="nav">
       <ul class="nav__list">
-        <li data-link="home" tabindex="-1" class="underline-an nav__list__item selected">Home</li>
-        <li data-link="movies" tabindex="-1" class="underline-an nav__list__item">Peliculas</li>
-        <li data-link="shows" tabindex="-1" class="underline-an nav__list__item">Series TV</li>
-        <li data-link="about" tabindex="-1" class="underline-an nav__list__item">Acerca de</li>
+        <li data-link="home" tabindex="-1" class="nav__list__item selected">Home</li>
+        <li data-link="movies" tabindex="-1" class="nav__list__item">Peliculas</li>
+        <li data-link="shows" tabindex="-1" class="nav__list__item">Series TV</li>
+        <li data-link="about" tabindex="-1" class="nav__list__item">Acerca de</li>
       </ul>
     </nav>
     `);

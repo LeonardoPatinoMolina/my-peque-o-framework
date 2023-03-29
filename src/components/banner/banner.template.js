45db4609f4e476385fcb0f917340c164ef6a6133
Leonardo <leonardopatino99@gmail.com>
2023-03-29 11:42:42 -0500

    arreglo de gif caido convertido en asset local

--- src/components/banner/banner.template.js
+++ src/components/banner/banner.template.js
@@ -7,7 +7,7 @@ export class BannerComponent extends Component{
     <div class="banner">
       <div class="banner__img_wrapper">
         <img class="banner__img"
-          src="https://gifimage.net/wp-content/uploads/2017/10/cuenta-regresiva-gif-13.gif" alt="cuenta regresiva de cine antiguo" draggable="false">
+          src="src/assets/cuenta_regresiva.gif" alt="cuenta regresiva de cine antiguo" draggable="false">
       </div>
       <p class="banner__paragraph">
         <span>¡Bienvenido a nuestro sitio de películas!</span> Aquí, encontrarás una amplia selección de obras de todo el mundo, organizadas en categorías fáciles de navegar. Nuestro catálogo incluye una gran variedad de entregas, desde dramas y comedias hasta películas de terror y acción, para satisfacer todos los gustos.

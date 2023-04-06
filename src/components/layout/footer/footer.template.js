46e62318f7cddb019eb6f5d3003fd871fb4914c9
Leonardo <leonardopatino99@gmail.com>
2023-04-06 13:57:56 -0500

    corrección de link a github

--- src/components/layout/footer/footer.template.js
+++ src/components/layout/footer/footer.template.js
@@ -10,7 +10,7 @@ export class FooterComponent extends Component{
         <li class="footer__list__item">${this.props.year}</li>
         <li class="footer__list__item">Leonardopatino99@gmail.com</li>
         <li class="footer__list__item">
-          <a href="https://github.com/LeoCHAD" target="_blank">LeoChad-GitHub</a>
+          <a href="https://github.com/LeonardoPatinoMolina" target="_blank">LeonardoPatinoMolina-GitHub</a>
         </li>
       </ul>
     </footer>
